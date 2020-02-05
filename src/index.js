58592545dbe0643ff6f8cbe97e7f0afe0a902d00
toda93 <dttong93@gmail.com>
2020-02-05 11:36:27 +0700

    sync

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import MysqlModel, {MysqlDataTypes, registerConnection} from './mysql';
+import MySQLDataRepository from './MySQLDataRepository';
 
 
 export {
 	registerConnection,
 	MysqlDataTypes,
-	MysqlModel
+	MysqlModel,
+	MySQLDataRepository
 }
